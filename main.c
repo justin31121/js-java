77bef8c42dba85a9d65bce65d36b6d6e17b6939b
Schartner <Justin.Schartner@kaldewei.com>
2023-02-13 16:10:19 +0100

    temp

--- main.c
+++ main.c
@@ -1,254 +1,287 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include <pthread.h>
-#include <math.h>
-
-#include "./sdl_c.h"
-
-#define PEN_IMPLEMENTATION
-#include "pen.h"
+#include <SDL2/SDL.h>
 
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
 
-#define WIDTH 800
-#define HEIGHT 600
+#define WIDTH 200
+#define HEIGHT 200
 
-uint32_t *source;
+typedef unsigned long u64;
+typedef long s64;
+typedef unsigned int u32;
+typedef int s32;
+typedef unsigned char u8;
+typedef char s8;
 
-#define BUFFER_CAP 1024
+typedef struct {
+    u8 *ascii;
+    s32 *ascii_x;
+    s32 *ascii_y;
+    s32 *ascii_w;
+    s32 *ascii_h;
+    u32 height;
+    u32 ascii_width;
+}Font;
 
-char buffer[BUFFER_CAP] = {0};
-size_t buffer_size = 0;
-
-SDL_Renderer *rend;
-SDL_Texture *texture;
-bool running = true;
-
-void render() {
-  SDL_RenderClear(rend);
-  SDL_RenderCopy(rend, texture, NULL, NULL);
-  SDL_RenderPresent(rend);
-}
-
-unsigned char ttf_buffer[1<<25];
-
-#define FONT_HEIGHT 22
-#define FONT_SPACE_WIDTH 12
-#define ASCII_VALS 127-32
-
-uint32_t ascii[ASCII_VALS][FONT_HEIGHT * FONT_HEIGHT];
-int ascii_w[ASCII_VALS];
-int ascii_h[ASCII_VALS];
-
-float estimate_word_len(stbtt_fontinfo *font, const char*word, size_t word_len) {
-  float xpos = 0.f;
-  float scale = stbtt_ScaleForPixelHeight(font, FONT_HEIGHT);
-
-  for(size_t k=0;k<word_len;k++) {
-    float x_shift = xpos - (float) floor(xpos);
-    int advance, lsb;
-    stbtt_GetCodepointHMetrics(font, word[k], &advance, &lsb);
-
-    int x0, y0, x1, y1;
-    stbtt_GetCodepointBitmapBoxSubpixel(font, word[k], scale, scale, x_shift, 0, &x0, &y0, &x1, &y1);
-
-    xpos += (advance * scale);
-    if(word[k+1]<=126) {
-      xpos += scale*stbtt_GetCodepointKernAdvance(font, word[k], word[k+1]);
+bool font_init(Font *font, const char *font_path, u32 font_height) {
+    if(!font) {
+	return false;
     }
-  }
 
-  return xpos;
-}
-
-void render_word_len(stbtt_fontinfo *font, const char *word, size_t word_len, float x, int y) {
-  float xpos = x;
-  float scale = stbtt_ScaleForPixelHeight(font, FONT_HEIGHT);
-
-  for(size_t k=0;k<word_len;k++) {
-    float x_shift = xpos - (float) floor(xpos);
-    int advance, lsb;
-    stbtt_GetCodepointHMetrics(font, word[k], &advance, &lsb);
-
-    int x0, y0, x1, y1;
-    stbtt_GetCodepointBitmapBoxSubpixel(font, word[k], scale, scale, x_shift, 0, &x0, &y0, &x1, &y1);
-
-    int w = ascii_w[word[k] - 32];
-    int h = ascii_h[word[k] - 32];
+    if(!font_path) {
+	return false;
+    }
     
-    copy(source, WIDTH, HEIGHT, ascii[word[k] - 32], w, h, (int) xpos + x0, y + y0, w, h);
-
-    xpos += (advance * scale);
-    if(word[k+1]<=126) {
-      xpos += scale*stbtt_GetCodepointKernAdvance(font, word[k], word[k+1]);
+    //READ FILE BEGIN
+    FILE *f = fopen(font_path, "rb");
+    if(!f) {
+	return false;
     }
-  }  
-} 
-
-void render_text_len(stbtt_fontinfo *font, const char* text, size_t text_len,
-		     int x, int y,
-		     int *_x, int *_y) {
-  (void) _x;
-  (void) _y;
-  float xpos = (float) x;
-  size_t i=0;
-  size_t last = 0;
-  while(i<text_len) {
-    while(i<text_len && text[i]!=' ') i++;
-    if(i<=text_len) {
-      //RENDER
-      printf("'%.*s'\n", (int) (i - last), buffer + last);
-      float __x = estimate_word_len(font, buffer+last, i - last);
-      xpos += __x;
-      if((int) xpos >= WIDTH) {
-	xpos = 0.f;
-	y+=2*FONT_HEIGHT;
-      }
-      render_word_len(font, buffer+last, i - last, xpos==0 ? 0 : xpos-__x, y);
-      last = i;
+    
+    if(fseek(f, 0, SEEK_END) < 0) {
+	fclose(f);
+	return false;
     }
-    while(i<text_len && text[i]==' ') {
-      i++;
-      last++;
-      xpos += (float) FONT_SPACE_WIDTH;
-      if((int) xpos >= WIDTH) {
-	xpos = 0.f;
-	y+=2*FONT_HEIGHT;
-      }
+
+    s64 m = ftell(f);
+    if(m < 0) {
+	fclose(f);
+	return false;
+    }  
+
+    if(fseek(f, 0, SEEK_SET) < 0) {
+	fclose(f);
+	return false;
     }
-  }
-}
 
-void render_text(stbtt_fontinfo *font, const char* text, int x, int y, int *_x, int *_y) {
-  render_text_len(font, text, strlen(text), x, y, _x, _y);
-}
+    u8 *ttf_buffer = (u8 *) malloc((u32) m + 1);
+    if(!ttf_buffer) {
+	fclose(f);
+	return false;
+    }
 
-void* loop(void *ptr) {
-  (void) ptr;
+    u64 _m = (u64) m;
+    u64 n = fread(ttf_buffer, 1, _m, f);
+    if(n != _m) {
+	fclose(f);
+	return false;
+    }
+    ttf_buffer[n] = 0;
+    fclose(f);
+    //READ FILE END
+    
+    stbtt_fontinfo font_info;        
+    if(!stbtt_InitFont(&font_info, ttf_buffer, stbtt_GetFontOffsetForIndex(ttf_buffer,0))) {
+	free(ttf_buffer);
+	return false;
+    }
 
-  fill(source, WIDTH, HEIGHT, 0xff181818);
+    //TODO: combine all mallocs
+    u32 ascii_vals = 128 - 32;
+    font->ascii = (u8 *) malloc(ascii_vals * font_height * font_height * sizeof(u8));
+    if(!font->ascii) {
+	free(ttf_buffer);
+	return false;
+    }
 
-  stbtt_fontinfo font;
-  fread(ttf_buffer, 1, 1<<25, fopen("c:/windows/fonts/consola.ttf", "rb"));
-  
-  stbtt_InitFont(&font, ttf_buffer, 0);
+    font->ascii_x = (s32 *) malloc(ascii_vals * sizeof(s32));
+    if(!font->ascii_x) {
+	free(font->ascii);
+	free(ttf_buffer);
+	return false;
+    }
 
-  float scale = stbtt_ScaleForPixelHeight(&font, FONT_HEIGHT);
+    font->ascii_y = (s32 *) malloc(ascii_vals * sizeof(s32));
+    if(!font->ascii_y) {
+	free(font->ascii);
+	free(font->ascii_x);
+	free(ttf_buffer);
+	return false;
+    }
 
-  for(int c=32;c<=126;c++) {
-    int w, h;
-    unsigned char *bitmap =
-      stbtt_GetCodepointBitmap(&font, 0, scale, c, &w, &h, 0, 0);
+    font->ascii_w = (s32 *) malloc(ascii_vals * sizeof(s32));
+    if(!font->ascii_w) {
+	free(font->ascii);
+	free(font->ascii_x);
+	free(font->ascii_y);
+	free(ttf_buffer);
+	return false;
+    }
 
-    for (int j=0; j < FONT_HEIGHT; ++j) {
-      for (int i=0; i < FONT_HEIGHT; ++i) {
-	if(i<w && j<h) {
-	  unsigned char d = bitmap[j*w+i];
-	  if(d) {
-	    ascii[c-32][j*w+i] = 0xff000000 | (d<<0*8) | (d<<1*8) | (d<<2*8);
-	    continue;
-	  }
+    font->ascii_h = (s32 *) malloc(ascii_vals * sizeof(s32));
+    if(!font->ascii_h) {
+	free(font->ascii);
+	free(font->ascii_x);
+	free(font->ascii_y);
+	free(font->ascii_w);
+	free(ttf_buffer);
+	return false;
+    }
+
+    float scale = stbtt_ScaleForPixelHeight(&font_info, font_height);
+    u32 ascii_width = ascii_vals * font_height;
+    s32 _font_height = (s32) font_height;
+    font->height = font_height;
+    font->ascii_width = ascii_width;
+
+    for(int c=32;c<=126;c++) {
+
+	u32 glyph_off = font_height * (c - 32);
+	
+	int w, h, x, y;
+	unsigned char *bitmap =
+	    stbtt_GetCodepointSDF(&font_info, scale, c,  0, 128, 256.0, &w, &h, &x, &y);
+	    //stbtt_GetCodepointBitmap(&font_info, 0, scale, c, &w, &h, &x, &y);
+
+	font->ascii_w[c-32] = w;
+	font->ascii_h[c-32] = h;
+	font->ascii_x[c-32] = x;
+	font->ascii_y[c-32] = y;
+
+	for (s32 j=0;j<_font_height;++j) {
+	    for(s32 i=0;i<_font_height;++i) {
+		if(bitmap && i<w && j<h) {
+		    unsigned char d = bitmap[j*w+i];
+		    font->ascii[j*ascii_width+glyph_off+i] = d;
+		} else {
+		    font->ascii[j*ascii_width+glyph_off+i] = 0x00;
+		}
+	    }
 	}
-	ascii[c-32][j*FONT_HEIGHT+i] = 0xff181818;
-      }
-    }
-    ascii_w[c-32] = w;
-    ascii_h[c-32] = h;
     
-    stbtt_FreeBitmap(bitmap, font.userdata);
-  }
+	if(bitmap) stbtt_FreeBitmap(bitmap, font_info.userdata);
+    }
 
-
-
-  while(running) {
-    fill(source, WIDTH, HEIGHT, 0x181818);
-    int x, y;
-    render_text_len(&font,
-		  buffer, buffer_size,
-		  0, 1*FONT_HEIGHT, &x, &y); 
-    render();
-    SDL_Delay(100);
-  }
-  
-  return NULL;
+    free(ttf_buffer);
+    return true;
 }
 
-int main(int argc, char** argv) {
-  (void) argc;
-  (void) argv;
+void font_free(Font *font) {
+    if(!font) {
+	return;
+    }
+    
+    free(font->ascii);
+    free(font->ascii_x);
+    free(font->ascii_y);
+    free(font->ascii_w);
+    free(font->ascii_h);
+}
 
-  scc(SDL_Init(SDL_INIT_VIDEO));
-
-  SDL_Window *wind = scp(SDL_CreateWindow(
-					  "Image",
-					  SDL_WINDOWPOS_CENTERED,
-					  SDL_WINDOWPOS_CENTERED,
-					  WIDTH, HEIGHT, 0));
-
-  rend = scp(SDL_CreateRenderer(
-				wind,
-				-1,
-				SDL_RENDERER_SOFTWARE));
-
-  texture = scp(SDL_CreateTexture(
-				  rend,
-				  SDL_PIXELFORMAT_RGBA32,
-				  SDL_TEXTUREACCESS_STREAMING,
-				  WIDTH,
-				  HEIGHT));
-
-  int texture_pitch;
-  void* texture_pixels = NULL;
-  scc(SDL_LockTexture(texture, NULL, &texture_pixels, &texture_pitch));
-
-  source = (uint32_t *) texture_pixels;
-
-  SDL_Event event;
-  size_t len;
-
-  pthread_t thread;
-  if(pthread_create(&thread, NULL, loop, NULL) < 0 ) {
-    fprintf(stderr, "ERROR: Can not create thread");
-    exit(1);
-  }
-
-  while(running) {
-    SDL_WaitEvent(&event);
-
-    switch(event.type) {
-    case SDL_KEYDOWN:
-      switch(event.key.keysym.sym) {
-      case SDLK_BACKSPACE:
-	buffer_size = buffer_size ? buffer_size-1 : buffer_size;
-      }
-      break;
-    case SDL_TEXTINPUT:
-      len = strlen(event.text.text);
-      if(buffer_size + len >= BUFFER_CAP) {
-	fprintf(stderr, "ERROR: Buffer-Overflow\n");
-	exit(1);
-      }
-      memcpy(buffer + buffer_size, event.text.text, len);
-      buffer_size += len;
-      break;
-    case SDL_QUIT:
-      running = false;
-      break;
+void render(u32 *data, const Font *font) {
+    for(u32 y=0;y<HEIGHT;y++) {
+	for(u32 x=0;x<WIDTH;x++) {
+	    data[y*WIDTH + x] = 0xff202020;
+	}
     }
 
-    render();
-  }
+    u32 y0 = font->height;
+    u32 x0 = 0;
 
-  SDL_UnlockTexture(texture);
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(rend);
-  SDL_DestroyWindow(wind);
+    const char *cstr = "yoyoyo";
+    int i=0;
+    while(cstr[i]) {
+	char c = cstr[i];
+	u32 glyph_off = (c - 32) * font->height;
+	for(u32 dy=0;dy<font->height;dy++) {
+	    for(u32 dx=0;dx<font->height;dx++) {
+		s32 x = (s32) x0 + (s32) dx + font->ascii_x[c -32];
+		s32 y = (s32) y0 + (s32) dy + font->ascii_y[c-32];
+		if(x<0 || x>=WIDTH || y<0 || y>=HEIGHT) continue;
+		u8 d = font->ascii[dy*font->ascii_width+glyph_off+dx];
+		if(!d) continue;
+		d = d * (0xff - 0x20) / 255 + 0x20;
+		data[y*WIDTH+x] = 0xff000000 | (d << 16) | (d << 8) | (d);
+	    }
+	}
+	x0 += font->ascii_w[c - 32] + font->ascii_x[c - 32];
+	i++;
+    }    
+}
 
-  SDL_Quit();
+int main(int argc, char **argv) {
+    (void) argc;
+    (void) argv;
 
-  pthread_join(thread, NULL);
-  
-  return 0;
+    Font font;
+    if(!font_init(&font, "c:/windows/fonts/arialbd.ttf", 64)) {
+	fprintf(stderr, "ERROR: Can not load font\n");
+	exit(1);
+    }
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+	fprintf(stderr, "SDL-ERROR: %s\n", SDL_GetError());
+	exit(1);
+    }
+
+    SDL_Window *window = NULL;
+    if(!(window = SDL_CreateWindow("Font", 3000, 400, WIDTH, HEIGHT, 0))) {
+	fprintf(stderr, "SDL-ERROR: %s\n", SDL_GetError());
+	exit(1);	
+    }
+
+    SDL_Renderer *renderer = NULL;
+    if(!(renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE))) {
+	fprintf(stderr, "SDL-ERROR: %s\n", SDL_GetError());
+	exit(1);	
+    }
+
+    SDL_Texture *texture = NULL;
+    if(!(texture = SDL_CreateTexture(renderer,
+				     SDL_PIXELFORMAT_RGBA32,
+				     SDL_TEXTUREACCESS_STREAMING,
+				     WIDTH, HEIGHT))) {
+	fprintf(stderr, "SDL-ERROR: %s\n", SDL_GetError());
+	exit(1);
+    }
+
+    bool quit = false;
+    SDL_Rect window_rect = {0, 0, WIDTH, HEIGHT};
+
+    SDL_Event event;
+    while(!quit) {
+	SDL_WaitEvent(&event);
+
+	switch(event.type) {
+	case SDL_QUIT: {
+	    quit = true;
+	} break;
+	case SDL_KEYDOWN: {
+	    if(event.key.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_q) {
+		quit = true;
+	    }
+	} break;
+	}
+
+	void *texture_data = NULL;
+	int texture_pitch = 0;
+	if(SDL_LockTexture(texture, &window_rect, &texture_data, &texture_pitch) < 0) {
+	    fprintf(stderr, "SDL-ERROR: %s\n", SDL_GetError());
+	    exit(1);
+	}
+	//DRAW
+	render((u32 *) texture_data, &font);
+	
+	SDL_UnlockTexture(texture);
+
+	//RENDER
+	if(SDL_RenderClear(renderer) < 0) {
+	    fprintf(stderr, "SDL-ERROR: %s\n", SDL_GetError());
+	    exit(1);	    
+	}
+	if(SDL_RenderCopy(renderer, texture, &window_rect, &window_rect) < 0) {
+	    fprintf(stderr, "SDL-ERROR: %s\n", SDL_GetError());
+	    exit(1);	    
+	}
+	SDL_RenderPresent(renderer);
+    }
+    
+    //SDL_DestroyTexture(texture);
+    //SDL_DestroyRenderer(renderer);
+    //SDL_DestroyWindow(window);
+    //SDL_Quit();
+    
+    return 0;
 }
